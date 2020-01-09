f7ec593e6d9cd596c20cba95ee953d3651ed43ff
rafalison avotra <avotra.rafalison@gmail.com>
2020-01-09 17:08:28 +0300

    list deroulant

--- Components/AddPerson.js
+++ Components/AddPerson.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, TextInput, AsyncStorage } from "react-native";
+import { View, Text, StyleSheet, TextInput, AsyncStorage } from "react-native";
 import Constants from 'expo-constants';
 import shuffle from "shuffle-array";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { FormLabel, FormInput, FormValidationMessage, Input, Button, Icon } from "react-native-elements";
+import { Dropdown } from 'react-native-material-dropdown';
+
 
 
 console.log("tafiditra page 1");
@@ -15,19 +19,18 @@ class Page1 extends React.Component {
       Prenom: '',
       Cat: "",
       Tel: "",
-      Tab: [],
     }
   }
 
   async componentDidMount() {
     try {
-      var Item =  await AsyncStorage.getItem('data');
-      if(Item){
+      var Item = await AsyncStorage.getItem('data');
+      if (Item) {
         data = JSON.parse(Item)
-      }else if(Item === null){
-        await AsyncStorage.setItem('data',"[]");
+      } else if (Item === null) {
+        await AsyncStorage.setItem('data', "[]");
       }
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
@@ -41,7 +44,7 @@ class Page1 extends React.Component {
       this.setState({ [key]: val })
     } else if (key === "Tel") {
       this.setState({ [key]: val })
-    }else if (key === "Pseudo") {
+    } else if (key === "Pseudo") {
       this.setState({ [key]: val })
     }
   };
@@ -89,15 +92,23 @@ class Page1 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput placeholder="pseudo" onChangeText={this.inputhandler('Pseudo')} />
-        <TextInput placeholder="Nom" onChangeText={this.inputhandler('Nom')} />
-        <TextInput placeholder="Prénom" onChangeText={this.inputhandler('Prenom')} />
-        <TextInput placeholder="catégorie" onChangeText={this.inputhandler('Cat')} />
-        <TextInput placeholder="n° Tel..." onChangeText={this.inputhandler('Tel')} />
-        <Button
-          title="Ajouter"
-          onPress={() => this.ValidHandler()}
-        />
+        <Input placeholder="Pseudo..." onChangeText={this.inputhandler('Pseudo')} label='Pseudo' errorMessage="Pseudo Trop court"/>
+        <Input placeholder="Nom..." onChangeText={this.inputhandler('Nom')} label="Nom"/>
+        <Input placeholder="Prenom..." onChangeText={this.inputhandler('Prenom')} label="Prenom"/><Text style={{paddingLeft: 10, fontWeight:"bold", fontSize:16, color: "#889098"}}>Catégorie</Text>
+        <Dropdown data = {[{value: "Friends/Family"},{value:"Recreation"},{value:"Organisation"},{value:"Geographics"},{value:"Socials"}]}  onChangeText={this.inputhandler('Cat')}/>
+        <Input placeholder="n° Tél..." onChangeText={this.inputhandler('Tel')} label="Numéro Téléphone"/>
+        <View style={{ paddingTop: 10, flexDirection: "row" }}>
+          <View style={{ width: "50%", paddingRight: 5, paddingRight: 5, alignItems: "center" }}>
+            <TouchableOpacity style={{ alignItems: "center" }} onPress={()=> this.props.navigation.navigate('Home')}>
+              <Text>Annuler</Text><Icon name="cancel" reverse color="#F67A19"/>
+            </TouchableOpacity>
+          </View>
+          <View style={{ width: "50%", paddingRight: 5, paddingRight: 5, alignItems: "center" }}>
+            <TouchableOpacity style={{ alignItems: "center" }}>
+              <Text>Ajouter</Text><Icon name="add" reverse color="#28E555" onPress={()=> this.ValidHandler()}/>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -109,7 +120,21 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: "#fff"
-  }
+  },
+  input: {
+    padding: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    marginLeft: 5,
+    marginRight: 5,
+
+  },
+  container2: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%"
+  },
 });
 
 export default Page1;
@@ -126,3 +151,30 @@ export default Page1;
     )}
   />
 </View>;*/
+
+/* <ScrollView>
+          <View >
+            <TextInput style={styles.input} placeholder="pseudo" onChangeText={this.inputhandler('Pseudo')} />
+            <TextInput style={styles.input} placeholder="Nom" onChangeText={this.inputhandler('Nom')} />
+            <TextInput style={styles.input} placeholder="Prénom" onChangeText={this.inputhandler('Prenom')} />
+            <TextInput style={styles.input} placeholder="catégorie" onChangeText={this.inputhandler('Cat')} />
+            <TextInput style={styles.input} placeholder="n° Tel..." onChangeText={this.inputhandler('Tel')} />
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.container2}>
+                <Button
+                  title="Annuler"
+                  onPress={() => this.ValidHandler()}
+                  color="#E56728"
+                />
+            </View>
+              <View style={styles.container2}>
+                <Button
+                color="#28E555"
+                  style={{ width: "50%" }}
+                  title="Ajouter"
+                  onPress={() => this.ValidHandler()}
+                />
+              </View>
+            </View>
+          </View>
+        </ScrollView> */
--- Components/Page2.js
+++ Components/Page2.js
@@ -1,12 +1,25 @@
 import React from "react";
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, AsyncStorage} from 'react-native';
+var Friends_List = [];
 
 console.log("page2")
 class Page2 extends React.Component {
+
+ getItem = async () => {
+    var data = await AsyncStorage.getItem('data');
+    data = JSON.parse(data)
+    const data_length = data.length;
+    while(data_length > 0){
+        if(data[data_length].Catégorie === "Friends/Family")
+    }
+
+    return data
+}
     render(){
+        {this.getItem()}
         return(
             <View>
-                <Text>Page2</Text>
+                
                 <Button title="Suivant" 
                         onPress={()=> this.props.navigation.navigate('Page3')}/>
             </View>
