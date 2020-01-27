c4bec57fe1b5dbbc11926c53628d182502c1f4b6
rafalison avotra <avotra.rafalison@gmail.com>
2020-01-27 10:21:14 +0300

    list

--- Components/ListPerson.js
+++ Components/ListPerson.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, SectionList, Text, AsyncStorage, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import Lists from "./Lists";
-
+import Constants from 'expo-constants';
 
 export default class ListPerson extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            affichage: false
         }
     }
 
@@ -16,54 +17,59 @@ export default class ListPerson extends React.Component {
             var lists = await AsyncStorage.getItem('data');
             lists = JSON.parse(lists);
             var list_length = lists.length;
-            if (this.props.category === "Friends") {
-                while (list_length > 0) {
-                    if (lists[list_length - 1].Catégorie === "Friends/Family") {
-                        List_Person = [...List_Person, lists[list_length - 1]];
-                    }
-                    list_length--;
-                }
-                this.setState({ data: List_Person })
+            if (list_length === 0) {
+                this.setState({ affichage: true })
             }
-            else if (this.props.category === "Recreations") {
-                List_Person = [];
-                while (list_length > 0) {
-                    if (lists[list_length - 1].Catégorie === "Recreations") {
-                        List_Person = [...List_Person, lists[list_length - 1]];
+            else if (list_length > 0) {
+                if (this.props.category === "Friends") {
+                    while (list_length > 0) {
+                        if (lists[list_length - 1].Catégorie === "Friends/Family") {
+                            List_Person = [...List_Person, lists[list_length - 1]];
+                        }
+                        list_length--;
                     }
-                    list_length--;
+                    this.setState({ data: List_Person })
                 }
-                this.setState({ data: List_Person })
-            }
-            else if (this.props.category === "Organisations") {
-                List_Person = [];
-                while (list_length > 0) {
-                    if (lists[list_length - 1].Catégorie === "Organisations") {
-                        List_Person = [...List_Person, lists[list_length - 1]];
+                else if (this.props.category === "Recreations") {
+                    List_Person = [];
+                    while (list_length > 0) {
+                        if (lists[list_length - 1].Catégorie === "Recreations") {
+                            List_Person = [...List_Person, lists[list_length - 1]];
+                        }
+                        list_length--;
                     }
-                    list_length--;
+                    this.setState({ data: List_Person })
                 }
-                this.setState({ data: List_Person })
-            }
-            else if (this.props.category === "Geographics") {
-                List_Person = [];
-                while (list_length > 0) {
-                    if (lists[list_length - 1].Catégorie === "Geographics") {
-                        List_Person = [...List_Person, lists[list_length - 1]];
+                else if (this.props.category === "Organisations") {
+                    List_Person = [];
+                    while (list_length > 0) {
+                        if (lists[list_length - 1].Catégorie === "Organisations") {
+                            List_Person = [...List_Person, lists[list_length - 1]];
+                        }
+                        list_length--;
                     }
-                    list_length--;
+                    this.setState({ data: List_Person })
                 }
-                this.setState({ data: List_Person })
-            }
-            else if (this.props.category === "Socials") {
-                List_Person = [];
-                while (list_length > 0) {
-                    if (lists[list_length - 1].Catégorie === "Socials") {
-                        List_Person = [...List_Person, lists[list_length - 1]];
+                else if (this.props.category === "Geographics") {
+                    List_Person = [];
+                    while (list_length > 0) {
+                        if (lists[list_length - 1].Catégorie === "Geographics") {
+                            List_Person = [...List_Person, lists[list_length - 1]];
+                        }
+                        list_length--;
                     }
-                    list_length--;
+                    this.setState({ data: List_Person })
                 }
-                this.setState({ data: List_Person })
+                else if (this.props.category === "Socials") {
+                    List_Person = [];
+                    while (list_length > 0) {
+                        if (lists[list_length - 1].Catégorie === "Socials") {
+                            List_Person = [...List_Person, lists[list_length - 1]];
+                        }
+                        list_length--;
+                    }
+                    this.setState({ data: List_Person })
+                }
             }
 
         } catch (error) {
@@ -74,19 +80,19 @@ export default class ListPerson extends React.Component {
     render() {
         return (
             <>
-                {this.state.data.map((item) => (
-                        <Lists Pseudo={item.Pseudo}
-                               id={item.id}
-                               Nom={item.Nom}
-                               Prenom={item.Prenom}
-                               Category={item.Catégorie}
-                               Tel={item.Tél}
-                               Apt={item.Apt}
-                               Rel={item.Rel}
-                               Moy={item.Moy}
-                               email={item.email}
-                               route= {this.props.route}
-                        />
+                {this.state.affichage ? <Text style={styles.Text}>Aucun Liste</Text> : this.state.data.map((item) => (
+                    <Lists Pseudo={item.Pseudo}
+                        id={item.id}
+                        Nom={item.Nom}
+                        Prenom={item.Prenom}
+                        Category={item.Catégorie}
+                        Tel={item.Tél}
+                        Apt={item.Apt}
+                        Rel={item.Rel}
+                        Moy={item.Moy}
+                        email={item.email}
+                        route={this.props.route}
+                    />
                 ))}
             </>
         )
@@ -105,6 +111,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
     },
-
+    Text: {
+        paddingTop: Constants.statusBarHeight,
+    }
 })
 var List_Person = [];
